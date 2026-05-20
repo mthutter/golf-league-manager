a77ab0c8da211e6db5f00f141cfed0ad2695ca20
Mark Hutter <mthutter@me.com>
2026-05-20 15:09:33 -0600

    adding initial route security/authentication

--- index.js
+++ index.js
@@ -8,28 +8,45 @@ import { MemoryStore } from "express-session";
 import fileUpload from "express-fileupload";
 import { v4 as uuid } from "uuid";
 import { MongoClient } from "mongodb";
+import sqlite3 from "sqlite3";
 import cookieParser from "cookie-parser";
+import jwt from "jsonwebtoken";
+import basicAuth from "express-basic-auth";
+import bcrypt from "bcryptjs";
+
 import homeController from "./controllers/home.js";
 import courseController from "./controllers/course.js";
 import videos2024Controller from "./controllers/videos2024.js";
 import videos2025Controller from "./controllers/videos2025.js";
-import videosController from "./controllers/videos.js";
 import teetimesController from "./controllers/tee-times.js";
 import addPlayerForm from "./controllers/add-player-form.js";
-import { showForm } from "./controllers/formController.js";
-import { submitForm } from "./controllers/formController.js";
-
-import firstHalfController from "./controllers/first-half.js";
-import secondHalfController from "./controllers/second-half.js";
 import overallController from "./controllers/overall.js";
 import rulesController from "./controllers/rules.js";
 import resultsController from "./controllers/results.js";
 import skinsController from "./controllers/skins.js";
-import * as dbmodel from "./models/dbModel.js";
 
-import sqlite3 from "sqlite3";
+app.use(cookieParser());
+require("dotenv").config();
 
 const app = new express();
+const SECRET_KEY = "secretkey";
+
+const authenticateToken = (req, res, next) => {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (!token) return res.sendStatus(401);
+
+  jwt.verify(token, SECRET_KEY, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+};
+
+app.get("/add-player-form", authenticateToken, (req, res) => {
+  res.json({ message: "Welcome....", user: req.user });
+});
 
 app.disable("x-powered-by");
 app.set("view engine", "ejs");
@@ -68,7 +85,7 @@ app.use(
     genid: (req) => {
       return uuid();
     },
-  })
+  }),
 );
 
 async function getFilenames(year) {
@@ -80,11 +97,10 @@ async function getFilenames(year) {
       password: "ac5c1048-f353-4655-8dbe4c573ee9-85ec-4a40",
       secure: false,
     });
-
     const list = await client.list(year + "/"); // FTP folder
     return list.map((item) => item.name); // Get names only
   } catch (err) {
-    console.error("FTP Error:", err);
+    console.error("FTP Connection Error:", err);
     return [];
   } finally {
     client.close();
@@ -106,16 +122,10 @@ app.get("/images2026", async (req, res) => {
   res.render("images2026", { filenames });
 });
 
-app.get("/images", async (req, res) => {
-  const filenames = await getFilenames("2025");
-  res.render("images", { filenames });
-});
-
 app.get("/", homeController);
 app.get("/course", courseController);
 app.get("/videos2024", videos2024Controller);
 app.get("/videos2025", videos2025Controller);
-app.get("/videos", videosController);
 app.get("/results", resultsController);
 app.get("/tee-times", teetimesController);
 app.get("/rules", rulesController);
@@ -127,7 +137,7 @@ const db = new sqlite3.Database("./golf-league-db.db", (err) => {
   console.log("Connected to the SQLite database.");
 });
 
-app.get("/add-player-form", addPlayerForm);
+//app.get("/add-player-form", addPlayerForm);
 
 app.post("/add-player", function (req, res) {
   const { name_last, name_first, phone, handicap, password, e_mail, year_joined, status, type } = req.body;
