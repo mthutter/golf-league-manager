b3bdc54f4e49664519505ef7108d2a08dbbfdef1
Mark Hutter <mthutter@me.com>
2026-05-20 16:23:49 -0600

    mods to index.js - enabled helmet, add basic auth to /add-player-form and the POST of the form data to SQL

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import ftp from "basic-ftp";
-import fs from "fs";
 import express from "express";
 import bodyParser from "body-parser";
 import flash from "connect-flash";
@@ -7,12 +6,13 @@ import expressSession from "express-session";
 import { MemoryStore } from "express-session";
 import fileUpload from "express-fileupload";
 import { v4 as uuid } from "uuid";
-import { MongoClient } from "mongodb";
 import sqlite3 from "sqlite3";
 import cookieParser from "cookie-parser";
 import jwt from "jsonwebtoken";
 import basicAuth from "express-basic-auth";
 import bcrypt from "bcryptjs";
+import dotenv from "dotenv";
+import helmet from "helmet";
 
 import homeController from "./controllers/home.js";
 import courseController from "./controllers/course.js";
@@ -25,27 +25,14 @@ import rulesController from "./controllers/rules.js";
 import resultsController from "./controllers/results.js";
 import skinsController from "./controllers/skins.js";
 
-app.use(cookieParser());
-require("dotenv").config();
-
 const app = new express();
-const SECRET_KEY = "secretkey";
 
-const authenticateToken = (req, res, next) => {
-  const authHeader = req.headers["authorization"];
-  const token = authHeader && authHeader.split(" ")[1];
+dotenv.config();
 
-  if (!token) return res.sendStatus(401);
-
-  jwt.verify(token, SECRET_KEY, (err, user) => {
-    if (err) return res.sendStatus(403);
-    req.user = user;
-    next();
-  });
-};
-
-app.get("/add-player-form", authenticateToken, (req, res) => {
-  res.json({ message: "Welcome....", user: req.user });
+app.use(cookieParser());
+app.use((req, res, next) => {
+  res.set("Cache-Control", "no-store");
+  next();
 });
 
 app.disable("x-powered-by");
@@ -56,28 +43,21 @@ app.use((req, res, next) => {
   next();
 });
 
-//app.use(helmet());
-//app.use(
-//helmet.contentSecurityPolicy({
-//directives: {
-//scriptSrc: ["'self' data:", "fonts.gstatic.com", "fonts.googleapis.com", "static.elfsight.com", "cdn.jsdelivr.net"],
-//connectSrc: ["'self'", "core.service.elfsight.com", "cdn.jsdelivr.net"],
-//imgSrc: ["'self' data:", "'self'", "fonts.gstatic.com", "fonts.googleapis.com", "cdn.jsdelivr.net/npm/bootstrap@5.3.3", "static.elfsight.com", "files.elfsightcdn.com", "www.w3.org"],
-//},
-//})
-//);
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+  })
+);
+
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cookieParser());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
 app.use(fileUpload());
 app.use(flash());
 app.use(
   expressSession({
     name: "SessionCookie",
-    cookie: { maxAge: 60000 },
+    cookie: { maxAge: 86400000 },
     secret: "woot",
     store: new MemoryStore({ checkPeriod: 86400000 }),
     resave: false,
@@ -85,16 +65,23 @@ app.use(
     genid: (req) => {
       return uuid();
     },
-  }),
+  })
 );
+
+//basic auth for 'app-player-form'
+const myAuth = basicAuth({
+  users: { [process.env.ADMIN_USER]: process.env.ADMIN_PASS },
+  challenge: true, // This triggers the browser login prompt
+  unauthorizedResponse: "Direct access denied.",
+});
 
 async function getFilenames(year) {
   const client = new ftp.Client();
   try {
     await client.access({
-      host: "la.storage.bunnycdn.com",
-      user: "bottoms-up",
-      password: "ac5c1048-f353-4655-8dbe4c573ee9-85ec-4a40",
+      host: process.env.BUNNY_HOST,
+      user: process.env.BUNNY_USER,
+      password: process.env.BUNNY_PASS,
       secure: false,
     });
     const list = await client.list(year + "/"); // FTP folder
@@ -139,7 +126,7 @@ const db = new sqlite3.Database("./golf-league-db.db", (err) => {
 
 //app.get("/add-player-form", addPlayerForm);
 
-app.post("/add-player", function (req, res) {
+app.post("/add-player", myAuth, function (req, res) {
   const { name_last, name_first, phone, handicap, password, e_mail, year_joined, status, type } = req.body;
   const sql = `INSERT INTO members (name_last, name_first, phone, handicap, password, e_mail, year_joined, status, type) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`;
 
@@ -156,7 +143,7 @@ app.post("/add-player", function (req, res) {
   });
 });
 
-app.get("/players", function (req, res) {
+app.get("/players", myAuth, function (req, res) {
   const sql = `
     SELECT
       id,
@@ -185,6 +172,11 @@ app.get("/players", function (req, res) {
       players: rows,
     });
   });
+});
+
+// Protect only the specific route
+app.get("/add-player-form", myAuth, (req, res) => {
+  res.render("add-player-form");
 });
 
 let port = process.env.PORT || 3000;
