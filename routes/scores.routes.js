4272ec33470235259a1b56492b4acbfdb5ebe00c
Mark Hutter <mthutter@me.com>
2026-05-28 17:48:43 -0600

    added code to pull data from 'members' into weekly scoring form/page

--- routes/scores.routes.js
+++ routes/scores.routes.js
@@ -1,3 +1,4 @@
+import db from "../config/db.js";
 import express from "express";
 import authMiddleware from "../middleware/auth.middleware.js";
 import { showWeeklyScoresForm } from "../controllers/scores.controller.js";
@@ -8,7 +9,24 @@ const router = express.Router();
    ADD WEEKLY SCORES FORM
 ========================================= */
 
-router.get("/new", authMiddleware, showWeeklyScoresForm);
+router.get("/new", authMiddleware, (req, res) => {
+  const sql = `
+    SELECT id, name_first, name_last
+    FROM members
+    ORDER BY name_last, name_first
+  `;
+
+  db.all(sql, [], (err, members) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send("Database error");
+    }
+
+    res.render("weekly-scores-form", {
+      members,
+    });
+  });
+});
 
 /* =========================================
    CREATE PLAYER
