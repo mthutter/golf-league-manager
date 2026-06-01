01887d6bfc87d67962cf81ecf5bd92f3c0d88596
Mark Hutter <mthutter@me.com>
2026-05-31 21:27:07 -0600

    added handicap calculation (run after scores entered for each week) "calculateHandicaps.js"

--- config/db.js
+++ config/db.js
@@ -6,4 +6,22 @@ const db = new sqlite3.Database("./golf-league-db.db", (err) => {
   console.log("Connected to SQLite");
 });
 
+export function all(sql, params = []) {
+  return new Promise((resolve, reject) => {
+    db.all(sql, params, (err, rows) => {
+      if (err) reject(err);
+      else resolve(rows);
+    });
+  });
+}
+
+export function run(sql, params = []) {
+  return new Promise((resolve, reject) => {
+    db.run(sql, params, function (err) {
+      if (err) reject(err);
+      else resolve(this);
+    });
+  });
+}
+
 export default db;
