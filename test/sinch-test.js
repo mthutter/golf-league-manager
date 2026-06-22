aa4ceb8755f76b2907331ee2742af06ebe4eeaa9
Mark Hutter <mthutter@me.com>
2026-06-22 18:42:39 +0000

    updated test js code - successful test message sent

--- test/sinch-test.js
+++ test/sinch-test.js
@@ -2,7 +2,7 @@ import { SinchClient } from "@sinch/sdk-core";
 
 const sinchClient = new SinchClient({
   // SMS Service Plan
-  projectId: "0ed0d43284944d27bfe3f5e983168b9f",
+  projectId: "dfa3761c-9068-4a74-b513-a0eeeefcae65",
   keyId: "9988ea55-b855-486f-bd4a-15d4570933ef",
   keySecret: "DQnZYLP~fnY.FN5rLzq0h~nX2c",
 });
@@ -10,7 +10,7 @@ const sinchClient = new SinchClient({
 async function run() {
   const response = await sinchClient.conversation.messages.send({
     sendMessageRequestBody: {
-      app_id: "0ed0d43284944d27bfe3f5e983168b9f",
+      app_id: "01KVR6F5TTXMMPNJ8J3PXQ5FSP",
       recipient: {
         identified_by: {
           channel_identities: [
