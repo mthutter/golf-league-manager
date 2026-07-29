e4a0ca1af96572a0b265cb3f9f5729a21b526401
Mark Hutter <mthutter@me.com>
2026-07-28 20:36:18 -0600

    added some logging to catch 'result' value

--- controllers/activation.controller.js
+++ controllers/activation.controller.js
@@ -22,6 +22,10 @@ export async function showActivationPage(req, res, next) {
 
     const result = await validateActivationToken(token);
 
+    console.log(result);
+    console.log(result.valid);
+    console.log(token);
+
     if (!result.valid) {
       return res.status(400).render("activation-invalid", {
         reason: result.reason,
