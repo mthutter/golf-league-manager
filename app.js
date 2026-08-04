8596ddfd50b5b006cf6fa48d966f21f7dd3f6fb7
Mark Hutter <mthutter@me.com>
2026-08-04 18:34:23 +0000

    removed console log output for troubleshooting

--- app.js
+++ app.js
@@ -144,9 +144,6 @@ app.use((req, res, next) => {
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
 
-  console.log("Req/User", req.user);
-  console.log("Res/Locals", res.locals);
-
   next();
 });
 
