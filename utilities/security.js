94fd8af8b33f9d4c667049d900ce9266766aa3dd
Mark Hutter <mthutter@me.com>
2026-08-19 14:26:49 +0000

    change ttl to 1 hr on NodeCache

--- utilities/security.js
+++ utilities/security.js
@@ -7,7 +7,7 @@ import posthogClient from "./posthog.js";
 
 // 1. Initialize self-cleaning ban cache (TTL: 24h, check expired keys every hour)
 const banCache = new NodeCache({
-  stdTTL: 86400,
+  stdTTL: 3600,
   checkperiod: 3600,
 });
 
